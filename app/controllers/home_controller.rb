df456f8ff9386ccea8ffadaa86bc043a1b22cf94
Fabiano Leite <fabianoleittes@gmail.com>
2016-07-26 22:04:38 -0300

    Refactoring home controller.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,18 +2,6 @@ class HomeController < ApplicationController
   def index
     @makes = Make.all
 
-    #search the make
-    uri = URI("http://www.webmotors.com.br/carro/marcas")
-
-    # Make request for Webmotors site
-    response = Net::HTTP.post_form(uri, {})
-    json = JSON.parse response.body
-
-    # Itera no resultado e grava as marcas que ainda não estão persistidas
-    json.each do |make_params|
-      if Make.where(name: make_params["Nome"]).size == 0
-        Make.create(name: make_params["Nome"], webmotors_id: make_params["Id"])
-      end
-    end
+    SearchMakes.fetch
   end
 end
